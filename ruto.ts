9bd4dd6e1ee38269b11362b2652a1c239d4c3e8f
Sylvie <sylvie@tycrek.com>
2025-03-03 19:12:57 -0700

    is better

--- ruto.ts
+++ ruto.ts
@@ -1,20 +1,39 @@
 import pkg from './deno.json' with { type: 'json' };
 import Log from '@tycrek/log';
 import { Hono } from '@hono/hono';
+import type { RedirectStatusCode } from '@hono/hono/utils/http-status';
+
+interface Route {
+	sources: string[];
+	destination: string;
+	code?: number;
+}
 
 const log = new Log({ prefix: `ruto v${pkg.version} |` });
-const app = new Hono<{ Variables: { 'domain': string } }>();
+const app = new Hono<{ Variables: { 'where': string } }>();
 
 app.use(async (ctx, next) => {
-	ctx.set('domain', new URL(ctx.req.url).origin);
+	const start = Date.now();
 	await next();
-	log.info(`[${ctx.res.status}] ${ctx.req.url}`);
+	const time = Date.now() - start;
+	
+	if (ctx.res.status == 404) log.warn(`[404] [${time}ms] ${ctx.req.url}`);
+	else log.success(`[${ctx.res.status}] [${Date.now() - start}ms] ${ctx.req.url} -> ${ctx.get('where')}`);
 });
+
 app.get('*', async (ctx) => {
-	const redirects = <{[key:string]: string}> JSON.parse(await Deno.readTextFile('./redirects.json'));
-	return !redirects[ctx.req.url]
-		? ctx.notFound()
-		: ctx.redirect(redirects[ctx.req.url]);
+	const routes = <Route[]> JSON.parse(await Deno.readTextFile('./redirects.json'));
+
+	for (const route of routes) {
+		for (const source of route.sources) {
+			if (ctx.req.url.match(source)) {
+				ctx.set('where', route.destination);
+				return ctx.redirect(route.destination, <RedirectStatusCode> route.code ?? 302);
+			}
+		}
+	}
+
+	return ctx.notFound();
 });
 
 log.info(`deno ${Deno.version.deno} typescript ${Deno.version.typescript}`);
